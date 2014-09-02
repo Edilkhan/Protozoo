382716bd889f123c40c96579435ea94ef590e103
wizardofos <wolfgang.vanos@gmail.com>
2014-09-02 17:31:21 +0200

    Removed iPOJO dependency

--- io/cxf/rest/src/main/java/org/woz/protozoa/io/cxf/rest/LocationServiceImpl.java
+++ io/cxf/rest/src/main/java/org/woz/protozoa/io/cxf/rest/LocationServiceImpl.java
@@ -7,34 +7,29 @@
 package org.woz.protozoa.io.cxf.rest;
 
 import java.util.List;
-import org.apache.felix.ipojo.annotations.Component;
-import org.apache.felix.ipojo.annotations.Instantiate;
-import org.apache.felix.ipojo.annotations.Requires;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.woz.protozoa.services.model.HashDB;
 import org.woz.protozoa.services.model.Location;
 
 /**
  *
  * @author wolfgang
  */
-@Component
-@Instantiate
 public class LocationServiceImpl implements LocationService {
     protected final Logger logger = LoggerFactory.getLogger(LocationServiceImpl.class);
-    @Requires
-    protected HashDB database;
+    private org.woz.protozoa.services.model.LocationService ls;
     
     @Override
     public Location getLocation(String name) {
-        return database.getLocation(name);
+        if (ls == null) {
+            System.out.println("Service has not been set");
+            return null;
+        }
+        return ls.getLocation(name);
     }
 
     @Override
     public List<Location> getLocations() {
         return null;
-    }
-    
-    
+    }    
 }
