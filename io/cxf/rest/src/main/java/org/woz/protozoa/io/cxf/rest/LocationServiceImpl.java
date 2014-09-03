f8d1cc8bdef3ae21df9b670fb9c26ace9e598bff
Wolfgang van Os <wolfgang@wizardofos.nl>
2014-09-03 22:28:11 +0200

    Removed obsolete imports

--- io/cxf/rest/src/main/java/org/woz/protozoa/io/cxf/rest/LocationServiceImpl.java
+++ io/cxf/rest/src/main/java/org/woz/protozoa/io/cxf/rest/LocationServiceImpl.java
@@ -6,10 +6,8 @@
 
 package org.woz.protozoa.io.cxf.rest;
 
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.woz.protozoa.services.model.Location;
 
 /**
  *
